e3bf53c9cc4b9c83ff66cf13f46ed7902d34068f
poorneshks <132078229+poorneshks@users.noreply.github.com>
2025-11-28 16:35:10 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('CI/CD Pipeline Test SUCCESSFUL! This is Version 2.0.'); // <-- UPDATED MESSAGE
+    res.send('CI/CD Pipeline Test SUCCESSFUL! This is Version ks poornesh pipeline'); // <-- UPDATED MESSAGE
 });
 
 const PORT = 3000;
