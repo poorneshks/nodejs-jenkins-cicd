5e392399992c701051c70c9ef8b62dd8e749082a
poorneshks <132078229+poorneshks@users.noreply.github.com>
2025-11-28 16:45:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,17 +1,9 @@
 const express = require('express');
-
 const app = express();
 
-
-
 app.get('/', (req, res) => {
-
-    res.send('Hello from Node.js CI/CD Pipeline using Jenkins & Azure VM!');
-
+    res.send('CI/CD Pipeline Test SUCCESSFUL! This is Version 2.0.'); 
 });
 
-
-
 const PORT = 3000;
-
-app.listen(PORT, () => console.log(`App running on port ${PORT}`)); 
+app.listen(PORT, () => console.log(`App running on port ${PORT}`));
